fc750fc717678529ec96f87e8fb1792b69b514fc
mugaruralbert@gmail.com <mugaruralbert@gmail.com>
2026-04-16 13:21:09 +0300

    The LoginScreen Done

--- app/src/main/java/com/campusnest1/groupq/navigation/AppNavHost.kt
+++ app/src/main/java/com/campusnest1/groupq/navigation/AppNavHost.kt
@@ -6,15 +6,36 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.campusnet.ui.LoginScreen
+import com.campusnest1.groupq.viewmodel.auth.loginViewModel
 
 @Composable
 fun AppNavHost() {
     val navController = rememberNavController()
+    val loginVM = loginViewModel()
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = Screen.Login.route
     ) {
+
+        composable(Screen.Login.route) {
+            LoginScreen(
+                onLoginClick = { email, password ->
+                    loginVM.login(email, password) { user ->
+                        if (user != null) {
+                            navController.navigate(Screen.Home.route) {
+                                popUpTo(Screen.Login.route) { inclusive = true }
+                            }
+                        }
+                    }
+                },
+                onSignUp = { navController.navigate(Screen.Register.route) },
+                onForgotPassword = {},
+                onGoogleSignIn = {},
+                onAppleSignIn = {}
+            )
+        }
 
         composable(Screen.Home.route) {
             HomeScreen(navController)
