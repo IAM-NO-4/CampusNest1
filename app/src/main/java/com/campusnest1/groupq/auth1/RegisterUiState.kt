d623932d775409e0ce7c2ded67d3962fbb02249a
Ibrahim Amir muwayire <muwayirei@gmail.com>
2026-04-16 17:48:16 +0300

    added ful register section.

---
+++ app/src/main/java/com/campusnest1/groupq/auth1/RegisterUiState.kt
@@ -0,0 +1,16 @@
+package com.campusnest1.groupq.auth1;
+
+data class RegisterUiState(
+    val name: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val password: String = "",
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val isSuccess: Boolean = false,
+    var passwordVisible: Boolean = false,
+
+    val nameError: String? = null,
+    val emailError: String? = null,
+    val passwordError: String? = null,
+)
